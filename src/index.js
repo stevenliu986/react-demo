e743ed296594b054cb752692803efac6648ac297
StevenLiu <stevenliu986@gmail.com>
2023-07-13 18:13:11 +1000

    new file:   src/index.css 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<div>React Demo</div>);
+root.render(<div className="header">React Demo</div>);
