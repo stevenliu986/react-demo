9ef9031878ae50842f8abe2ec65af4aa03bc8d02
StevenLiu <stevenliu986@gmail.com>
2023-07-14 20:10:21 +1000

    modified:   src/components/DemoOne.jsx

--- src/components/DemoOne.jsx
+++ src/components/DemoOne.jsx
@@ -1,16 +1,21 @@
 import PropTypes from "prop-types";
+import React from "react";
 
 const DemoOne = (props) => {
   let { title, x, children } = props;
 
-  // 要对children进行处理
-  if (!children) {
-    // 如果children没有传，将children变为空数组
-    children = [];
-  } else if (!Array.isArray(children)) {
-    // 如何children只传了一个，即不是数组，将children变为只有一个元素的数组
-    children = [children];
-  }
+  // // 要对children进行处理，自定义的方法
+  // if (!children) {
+  //   // 如果children没有传，将children变为空数组
+  //   children = [];
+  // } else if (!Array.isArray(children)) {
+  //   // 如何children只传了一个，即不是数组，将children变为只有一个元素的数组
+  //   children = [children];
+  // }
+
+  // 可以基于React.Children对象中提供的方法对props.children进行处理，方法有：count/foreach/map/toArray等
+  // 好处：在这些方法的内部，已经对children的各种形式做了处理
+  children = React.Children.toArray(children);
   return (
     <div className="box">
       {children[0]}
