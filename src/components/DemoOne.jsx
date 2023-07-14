a7dda6983945f10cd8a3aa57b93ab7adc03a460d
StevenLiu <stevenliu986@gmail.com>
2023-07-14 19:53:17 +1000

    add children

--- src/components/DemoOne.jsx
+++ src/components/DemoOne.jsx
@@ -1,11 +1,22 @@
 import PropTypes from "prop-types";
 
 const DemoOne = (props) => {
-  const { title, x } = props;
+  let { title, x, children } = props;
+
+  // 要对children进行处理
+  if (!children) {
+    // 如果children没有传，将children变为空数组
+    children = [];
+  } else if (!Array.isArray(children)) {
+    // 如何children只传了一个，即不是数组，将children变为只有一个元素的数组
+    children = [children];
+  }
   return (
     <div className="box">
+      {children[0]}
       <h2>{title}</h2>
       <h3>{x}</h3>
+      {children[children.length - 1]}
     </div>
   );
 };
