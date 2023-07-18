16892f8d80b030d0429d767eddfae2754f8fbfe0
StevenLiu <stevenliu986@gmail.com>
2023-07-18 15:09:21 +1000

    modified:   src/components/Demo4.jsx

--- src/components/Demo4.jsx
+++ src/components/Demo4.jsx
@@ -1,13 +1,20 @@
 import { Button } from "antd";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Demo1.scss";
 
 const Demo = () => {
   let [num, setNum] = useState(0);
 
+  // 函数组件中，还可以基于useRef来创建一个ref对象
+  let box = useRef(null);
+  console.log(box);
+  useEffect(() => {}, []);
+
   return (
     <div className="demo">
-      <span className="num">{num}</span>
+      <span className="num" ref={num}>
+        {num}
+      </span>
       <Button
         type="primary"
         size="small"
@@ -21,4 +28,29 @@ const Demo = () => {
   );
 };
 
+/*
+  // 可以基于React.createRef创建ref对象来获取想要的内容
+  let box = React.createRef();
+  useEffect(() => {
+    console.log(box.current);
+  }, []);
+
+  return (
+    <div className="demo">
+      <span className="num" ref={box}>
+        {num}
+      </span>
+      <Button
+        type="primary"
+        size="small"
+        onClick={() => {
+          setNum(num + 1);
+        }}
+      >
+        新增
+      </Button>
+    </div>
+  );
+};
+*/
 export default Demo;
