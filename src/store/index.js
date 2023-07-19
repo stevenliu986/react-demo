83420babc4e82af63a5294876a9dfa05fd9a9174
StevenLiu <stevenliu986@gmail.com>
2023-07-19 19:47:39 +1000

    modified:   src/store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,12 @@
 // 创建redux的公式容器
+import { createStore } from "redux";
+
+let initialState = { supNum: 10, oppNum: 5 };
+// 创建reducer，来修改store容器中的公共状态
+const reducer = (state = initialState, action) => {
+  // state: 存储store容器中的公共状态，初始值是initialState
+  // action: 每一次基于dispatcher派发的时候，传递进来的行为对象
+};
+
+// 创建store公共容器
+const store = createStore(reducer);
